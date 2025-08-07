edf183df296da99c9068ab4205d0dd03fc488231
Luke Ruter <luke@laar.us>
2025-08-07 16:48:12 -0400

    Fixed potential crash in NFC android interaction

--- android/app/src/main/java/com/psia/pkoc/TLVProvider.java
+++ android/app/src/main/java/com/psia/pkoc/TLVProvider.java
@@ -2,6 +2,8 @@ package com.psia.pkoc;
 
 import static java.lang.System.arraycopy;
 
+import android.util.Log;
+
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.util.Arrays;
@@ -48,9 +50,26 @@ public class TLVProvider
     public static BLE_Packet GetBleValue(byte[] encodedData)
     {
         BLE_PacketType packetType = BLE_PacketType.decode(encodedData[0]);
+        int length = encodedData[1] & 0xFF;
 
-        byte[] decodedData = new byte[encodedData[1]];
-        arraycopy(encodedData, 2, decodedData, 0, encodedData[1]);
+        Log.d("TLVProvider", "Processing TLV: Type=" + packetType + ", Length=" + length);
+
+        if (packetType == BLE_PacketType.Void)
+        {
+            Log.d("TLVProvider", "Skipping Void packet");
+            return null;
+        }
+
+        if (length > encodedData.length - 2)
+        {
+            Log.e("TLVProvider", "Invalid length: " + length + " for data: " + java.util.Arrays.toString(encodedData));
+            return null;
+        }
+
+        byte[] decodedData = new byte[length];
+        System.arraycopy(encodedData, 2, decodedData, 0, length);
+
+        Log.d("TLVProvider", "Decoded data: " + java.util.Arrays.toString(decodedData));
 
         return new BLE_Packet(packetType, decodedData);
     }
@@ -64,9 +83,26 @@ public class TLVProvider
     {
         NFC_PacketType packetType = NFC_PacketType.decode(encodedData[0]);
 
-        byte[] decodedData = new byte[encodedData[1]];
-        arraycopy(encodedData, 2, decodedData, 0, encodedData[1]);
+        int length = encodedData[1] & 0xFF;
 
+        Log.d("TLVProvider", "Processing TLV: Type=" + packetType + ", Length=" + length);
+
+        if (packetType == NFC_PacketType.Void)
+        {
+            Log.d("TLVProvider", "Skipping Void packet");
+            return null;
+        }
+
+        if (length > encodedData.length - 2)
+        {
+            Log.e("TLVProvider", "Invalid length: " + length + " for data: " + java.util.Arrays.toString(encodedData));
+            return null;
+        }
+
+        byte[] decodedData = new byte[length];
+        System.arraycopy(encodedData, 2, decodedData, 0, length);
+
+        Log.d("TLVProvider", "Decoded data: " + java.util.Arrays.toString(decodedData));
         return new NFC_Packet(packetType, decodedData);
     }
 
@@ -80,7 +116,7 @@ public class TLVProvider
     {
         ArrayList<BLE_Packet> gattTypeDataArrayList = new ArrayList<>();
 
-        if (data.length < 2) //not long enough to be a TLV
+        if (data.length < 3) // not long enough to be a TLV
         {
             return gattTypeDataArrayList;
         }
@@ -89,10 +125,27 @@ public class TLVProvider
         do
         {
             byte[] dataToProcess = new byte[data.length - processedDataLength];
-            arraycopy(data, processedDataLength, dataToProcess, 0, data.length - processedDataLength);
+            System.arraycopy(data, processedDataLength, dataToProcess, 0, data.length - processedDataLength);
+
+            // Debugging: Log the data being processed
+            Log.d("TLVProvider", "Data to process: " + java.util.Arrays.toString(dataToProcess));
+
             BLE_Packet gTypeData = GetBleValue(dataToProcess);
-            gattTypeDataArrayList.add(gTypeData);
-            processedDataLength += gTypeData.Data.length + 2;
+            if (gTypeData != null)
+            {
+                gattTypeDataArrayList.add(gTypeData);
+
+                // Debugging: Log the processed data length
+                Log.d("TLVProvider", "Processed data length: " + processedDataLength + ", Packet length: " + gTypeData.Data.length);
+
+                processedDataLength += gTypeData.Data.length + 2;
+            }
+            else
+            {
+                // we have either found an unrecognized TLV or have happened upon an incorrectly
+                // formatted TLV. Regardless, there is not more usable data left to process
+                return gattTypeDataArrayList;
+            }
         }
         while (processedDataLength < data.length);
 
@@ -119,8 +172,17 @@ public class TLVProvider
             byte[] dataToProcess = new byte[data.length - processedDataLength];
             arraycopy(data, processedDataLength, dataToProcess, 0, data.length - processedDataLength);
             NFC_Packet gTypeData = GetNfcValue(dataToProcess);
-            gattTypeDataArrayList.add(gTypeData);
-            processedDataLength += gTypeData.Data.length + 2;
+            if (gTypeData != null)
+            {
+                gattTypeDataArrayList.add(gTypeData);
+                processedDataLength += gTypeData.Data.length + 2;
+            }
+            else
+            {
+                // we have either found an unrecognized TLV or have happened upon an incorrectly
+                // formatted TLV. Regardless, there is not more usable data left to process
+                return gattTypeDataArrayList;
+            }
         }
         while (processedDataLength < data.length);
 
