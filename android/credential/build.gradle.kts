aeda831ca04f9f2f01dd84546196de8bc017e985
Rafa Ruiz <raruizmu@gmail.com>
2026-03-03 09:23:21 +0100

    Tests, back to the package name and README.md for deeplinks.

--- android/credential/build.gradle.kts
+++ android/credential/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.sentryinteractive.pkoc.testapp"
+        applicationId = "com.psia.pkoc"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
